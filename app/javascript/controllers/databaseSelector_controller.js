3a0c74b78efdbdc16554b50a54643f7874224646
watasuke102 <watasuke102@gmail.com>
2025-07-30 00:50:50 +0900

    update: remove unnecessary JS file

--- app/javascript/controllers/databaseSelector_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-export default class extends Controller {
-  connect() {
-    document.getElementById('database_select')?.addEventListener('change', (event) => {
-      Turbo.visit('/app/database/' + event.target.value);
-    })
-  }
-}
