26f1dc2e627265f9031bc21370dfa0feebb71ba3
watasuke102 <watasuke102@gmail.com>
2025-08-01 15:49:57 +0900

    update: add margin around form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,6 @@
 class UsersController < ApplicationController
   allow_unauthenticated_access only: %i[ new create ]
-  before_action :set_user, only: %i[ show update destroy ]
-
-  # GET /users or /users.json
-  def index
-    @users = User.all
-  end
-
-  # GET /users/1 or /users/1.json
-  def show
-  end
+  before_action :set_user, only: %i[ update destroy ]
 
   # GET /users/new
   def new
